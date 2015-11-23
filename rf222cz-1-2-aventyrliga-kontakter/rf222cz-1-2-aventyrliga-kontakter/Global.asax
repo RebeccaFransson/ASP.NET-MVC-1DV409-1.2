a18e4f2642de6b0773377b4b01ef9064a59fda7a
rf222cz <rf222cz@student.lnu.se>
2015-11-23 13:38:16 +0100

    first commit

---
+++ rf222cz-1-2-aventyrliga-kontakter/rf222cz-1-2-aventyrliga-kontakter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="rf222cz_1_2_aventyrliga_kontakter.MvcApplication" Language="C#" %>
---
+++ rf222cz-1-2-aventyrliga-kontakter/rf222cz-1-2-aventyrliga-kontakter/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace rf222cz_1_2_aventyrliga_kontakter
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
